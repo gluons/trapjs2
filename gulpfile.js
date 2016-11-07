f5163f48850d5dc3f236c2732b35f2d16f141719
Saran Tanpituckpong <sarunta@gmail.com>
2016-11-07 15:26:55 +0700

    Move build script into gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,63 @@
 const gulp = require('gulp');
 
 const del = require('del');
-
-const build = require('./build/build');
-const watch = require('./build/watch');
+const gutil = require('gulp-util');
+const webpack = require('webpack');
 
 gulp.task('clean', () => {
 	return del(['dist/*']);
 });
 
-gulp.task('build', ['clean'], () => {
-	return Promise.all([
-		build(require('./webpack.config')),
-		build(require('./webpack.prod.config'))
-	]);
+gulp.task('build:dist', ['clean'], (cb) => {
+	webpack(require('./webpack.config'), (err, stats) => {
+		if (err) {
+			throw new gutil.PluginError("webpack", err);
+		}
+		gutil.log("[webpack]", stats.toString({
+			colors: true
+		}));
+		cb();
+	});
 });
 
-gulp.task('watch', ['build'], () => {
-	watch(require('./webpack.config'));
-	watch(require('./webpack.prod.config'));
+gulp.task('build:min', ['clean'], (cb) => {
+	webpack(require('./webpack.prod.config'), (err, stats) => {
+		if (err) {
+			throw new gutil.PluginError("webpack", err);
+		}
+		gutil.log("[webpack]", stats.toString({
+			colors: true
+		}));
+		cb();
+	});
 });
+
+gulp.task('build', ['build:dist', 'build:min']);
+
+gulp.task('watch:dist', ['build:dist'], () => {
+	let compiler = webpack(require('./webpack.config'));
+	compiler.watch(null, (err, stats) => {
+		if (err) {
+			throw new gutil.PluginError("webpack", err);
+		}
+		gutil.log("[webpack]", stats.toString({
+			colors: true
+		}));
+	});
+});
+
+gulp.task('watch:min', ['build:min'], () => {
+	let compiler = webpack(require('./webpack.prod.config'));
+	compiler.watch(null, (err, stats) => {
+		if (err) {
+			throw new gutil.PluginError("webpack", err);
+		}
+		gutil.log("[webpack]", stats.toString({
+			colors: true
+		}));
+	});
+});
+
+gulp.task('watch', ['watch:dist', 'watch:min']);
 
 gulp.task('default', ['watch']);
